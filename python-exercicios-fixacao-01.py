502ae43a24238da396651ffd988ddc16466065fb
lcda-professor <leonardo.cdaraujo@professor.educacao.pe.gov.br>
2025-03-26 14:43:43 -0300

    Criado arquivo com respostas

---
+++ python-exercicios-fixacao-01.py
@@ -0,0 +1,29 @@
+#Questão 1
+
+#Questão 2
+
+#Questão 3
+
+#Questão 4
+
+#Questão 5
+
+#Questão 6
+
+#Questão 7
+
+#Questão 8
+
+#Questão 9
+
+#Questão 10
+
+#Questão 11
+
+#Questão 12
+
+#Questão 13
+
+#Questão 14
+
+#Questão 15
